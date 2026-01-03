f26e2d1ea83b2ab5f3d059bd030da9f9d6dedfde
Vishal-Kumar-Shaw <vkshaw1997@gmail.com>
2026-01-03 13:29:40 +0530

    Day 29 - ngIf, ngFor, ngSwitch

--- day28/interpolationApp/src/app/app.ts
+++ day28/interpolationApp/src/app/app.ts
@@ -1,19 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('interpolationApp');
   name = 'Vishal Kumar Shaw';
-  isDisabled = false;
+  isDisabled = true;
   value = 0;
   inputVal = "Test"
+  IsLoggedIn:boolean = false;
+  users = ['Vishal', 'Rahul', 'Amit'];
+  counterVal:number = 5;
+
+
   increament() {
     this.value++;
   }
@@ -22,4 +28,11 @@ export class App {
       this.value--;
     }
   }
+  login(){
+    this.IsLoggedIn = true;
+  }
+  logout(){
+    this.IsLoggedIn = false;
+  }
+
 }
