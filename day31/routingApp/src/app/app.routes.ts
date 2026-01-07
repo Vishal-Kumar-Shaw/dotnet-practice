1cc2c8958cb1f35a6b45da27356d7f4d3aad3f84
Vishal-Kumar-Shaw <vkshaw1997@gmail.com>
2026-01-07 23:40:43 +0530

    Day 35: AuthGurad simplest implementation

--- day31/routingApp/src/app/app.routes.ts
+++ day31/routingApp/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { authGuard } from './authGuard/auth.guard';
 
 export const routes: Routes = [
    { path: '', component: HomeComponent},
    { path: 'users', component: UsersComponent},
-   { path: 'users/:id', component: UserDetailComponent}
+   { path: 'home', component: HomeComponent},
+   { path: 'users/:id', component: UserDetailComponent, canActivate:[authGuard]}
 ];
--- day31/routingApp/src/app/services/auth-service.ts
+++ day31/routingApp/src/app/services/auth-service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthService {
-  IsLoggedIn:boolean = false;
+  IsLoggedIn():boolean{
+    return true;
+  }
 
 }
